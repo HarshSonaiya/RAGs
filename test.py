03ae7643482a7ac974f915af2838062ced140763
Harsh Sonaiya <harshsonaiya09@gmail.com>
2024-09-14 19:57:26 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from qdrant_client import QdrantClient
-
-client = QdrantClient(url="http://localhost:6333")  # Replace with your Qdrant URL
-collection_name = "rag"
-
-print(client.get_collection(collection_name=f"{collection_name}"))
-
